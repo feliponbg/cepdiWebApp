640f2e359351fd1c017d0ed4442d02c956fb477e
LAPTOP-VNHGS83I\felip <felipevb2017@outlook.com>
2023-02-19 18:18:50 -0600

    mejora 1

--- cepdiWebApp/Controllers/HomeController.cs
+++ cepdiWebApp/Controllers/HomeController.cs
@@ -47,10 +47,10 @@ namespace cepdiWebApp.Controllers
             return View();
         }
 
-        //public IActionResult Privacy()
-        //{
-        //    return View();
-        //}
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
